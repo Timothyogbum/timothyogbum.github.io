5e3591ba14b7c3f4197e9a27c224a8b1199c9cea
Timothy <oogbum@gmail.com>
2025-12-22 11:35:18 -0500

    README

---
+++ PYTHON/Practice.py
@@ -0,0 +1,11 @@
+# Type your code below
+my_var_1 = 'Hello World!'
+my_var_2 = 3.14
+name = "Bob"
+
+# Don't change the line below
+print(f"Variable 2 = {my_var_2}" )
+print(type(my_var_2))
+print(f"name = {name}")
+
+isinstance('Hello world', str)
